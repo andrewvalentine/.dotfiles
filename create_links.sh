025062d08dfe5aacdd1c2c59abbfd48b1138937d
AR Valentine <av13121@it000492.wks.bris.ac.uk>
2015-04-14 15:02:23 +0100

    remove . from .ssh/config and setup create_links.sh

---
+++ create_links.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ln -s /Users/$USER/.dotfiles/.alias $HOME/
+ln -s /Users/$USER/.dotfiles/.bash_profile $HOME/
+ln -s /Users/$USER/.dotfiles/.ssh/ $HOME/
+ln -s /Users/$USER/.dotfiles/.tmux.conf $HOME/
+ln -s /Users/$USER/.dotfiles/.vim $HOME/
+ln -s /Users/$USER/.dotfiles/.vimrc $HOME/
