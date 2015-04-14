240e21df387e56e764a2beb9ff90abd8af7e0930
Andrew Valentine <andrew.valentine@bristol.ac.uk>
2015-04-14 15:49:16 +0100

    add iterm2 plist and edit create_links to setup symbolic link

--- create_links.sh
+++ create_links.sh
@@ -5,3 +5,4 @@ ln -s /Users/$USER/.dotfiles/.ssh/ $HOME/
 ln -s /Users/$USER/.dotfiles/.tmux.conf $HOME/
 ln -s /Users/$USER/.dotfiles/.vim $HOME/
 ln -s /Users/$USER/.dotfiles/.vimrc $HOME/
+ln -s /Users/$USER/.dotfiles/com.googlecode.iterm2.plist $HOME/Library/Preferences/
