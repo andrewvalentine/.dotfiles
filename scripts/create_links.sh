e04f607ee97b511e6b8a97d9efec0bef207643e9
AV <andrew.r.valentine@gmail.com>
2015-11-24 21:14:15 +0000

    mod create links to include .bashrc

--- scripts/create_links.sh
+++ scripts/create_links.sh
@@ -5,3 +5,4 @@ ln -sf $HOME/.dotfiles/.ssh/ $HOME/
 ln -sf $HOME/.dotfiles/.tmux.conf $HOME/
 ln -sf $HOME/.dotfiles/.vim $HOME/
 ln -sf $HOME/.dotfiles/.vimrc $HOME/
+ln -sf $HOME/.dotfiles/.bashrc $HOME/
