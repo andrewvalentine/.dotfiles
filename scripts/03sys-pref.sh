cf9c943bc29ca23fd4a609699a154a8d8df3e0cf
AR Valentine <andrew.r.valentine@gmail.com>
2016-01-27 09:04:04 +0000

    add loads of stuff and improve formatting

--- scripts/03sys-pref.sh
+++ scripts/03sys-pref.sh
@@ -1,37 +1,39 @@
 #!/bin/bash
 
-#Enable trackpad tap to click
-#Requires log out/in cycle to take effect
-
+## UI configuration
+# Enable trackpad tap to click
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
-
-#Disable user interface sound effects
+## Sound
+# Disable user interface sound effects
 defaults write com.apple.systemsound "com.apple.sound.uiaudio.enabled" -int 0
 
-#Add bris.ac.uk search domains
+## Network configuration
+# Add bris.ac.uk search domains
 networksetup -setsearchdomains Ethernet users.bris.ac.uk isys.bris.ac.uk fen.bris.ac.uk cs.bris.ac.uk een.bris.ac.uk enm.bris.ac.uk cen.bris.ac.uk men.bris.ac.uk bris.ac.uk
 networksetup -setsearchdomains Wi-Fi users.bris.ac.uk isys.bris.ac.uk fen.bris.ac.uk cs.bris.ac.uk een.bris.ac.uk enm.bris.ac.uk cen.bris.ac.uk men.bris.ac.uk bris.ac.uk
 networksetup -setsearchdomains "Thunderbolt Ethernet Slot 1" users.bris.ac.uk isys.bris.ac.uk fen.bris.ac.uk cs.bris.ac.uk een.bris.ac.uk enm.bris.ac.uk cen.bris.ac.uk men.bris.ac.uk bris.ac.uk
 
-#Turn off Bluetooth
+# Turn off Bluetooth
 defaults write com.apple.Bluetooth ControllerPowerState 0
 
-#Customize data/time format
+## Date/time configuration
 defaults write com.apple.menuextra.clock DateFormat = "d MMM  HH:mm"
 defaults write com.apple.menuextra.clock FlashDateSeparators -int 0
 defaults write com.apple.menuextra.clock IsAnalog -int 0
 
-#Autohide Dock
+## Dock configuration
+# autohide
 defaults write com.apple.dock autohide -int 1 && killall Dock
 
-##Finder
-#Set New Window Target to $HOME
+## Finder configuration
+# Set New Window Target to $HOME
 defaults write com.apple.finder NewWindowTarget -string "PfHm"
-#Set Column View as default
+# Set Column View as default
 defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
+
 CFPreferencesAppSynchronize "com.apple.finder"
 
 echo "Some changes need a restart..."
