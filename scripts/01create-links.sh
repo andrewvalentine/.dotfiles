48dc27d3f4c9ac0f8228c99312d9bd8188fa1134
Andrew Valentine <andrew.r.valentine@gmail.com>
2016-04-02 07:14:44 +0100

    add .cmds link

--- scripts/01create-links.sh
+++ scripts/01create-links.sh
@@ -4,4 +4,5 @@ ln -sf $HOME/.dotfiles/.tmux.conf $HOME/
 ln -sf $HOME/.dotfiles/.vim $HOME/
 ln -sf $HOME/.dotfiles/.vimrc $HOME/
 ln -sf $HOME/.dotfiles/.bashrc $HOME/
+ln -sf $HOME/.dotfiles/.cmds $HOME/
 ln -sf /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/local/sbin/airport
