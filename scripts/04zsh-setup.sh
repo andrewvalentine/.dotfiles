d283345f9681b32ba4d3d66c773bd041d64a1941
andrewvalentine <andrew.r.valentine@gmail.com>
2016-05-24 21:32:06 +0100

    re-arrange execution order

--- scripts/04zsh-setup.sh
+++ scripts/04zsh-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-
 git clone https://github.com/olivierverdier/zsh-git-prompt.git $HOME/.zsh-git-prompt
+
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 ln -sF $HOME/.dotfiles/.zshrc $HOME/
