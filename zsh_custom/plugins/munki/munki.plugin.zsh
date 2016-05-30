048033d76ac68582a5b775224f7c52b547d344f2
andrewvalentine <andrew.r.valentine@gmail.com>
2016-05-30 21:57:19 +0100

    initial commit of zsh_custom dir including munki and pkgsrc zsh plugins

---
+++ zsh_custom/plugins/munki/munki.plugin.zsh
@@ -0,0 +1,17 @@
+#--------------------------------
+# Munki aliases
+#--------------------------------
+
+# Managed Software Update
+alias msu="sudo managedsoftwareupdate"
+alias msua="sudo managedsoftwareupdate --auto"
+alias msuv="sudo managedsoftwareupdate -vvv"
+alias msum="sudo managedsoftwareupdate --munkipkgsonly"
+alias msus="sudo managedsoftwareupdate --applesuspkgsonly"
+alias msui="sudo managedsoftwareupdate --installonly"
+
+# Logging
+alias msulog="tail -f /Library/Managed\ Installs/Logs/ManagedSoftwareUpdate.log"
+
+# Preferences
+alias msup="defaults read /Library/Preferences/ManagedInstalls"
