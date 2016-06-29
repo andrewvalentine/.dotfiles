0bc0493618611ca8a377edd9e158569154cc0ffe
andrewvalentine <andrew.r.valentine@gmail.com>
2016-06-29 21:23:27 +0100

    add .zshrc

--- scripts/01sym-links.sh
+++ scripts/01sym-links.sh
@@ -5,4 +5,5 @@ ln -sf $HOME/.dotfiles/.vim $HOME/
 ln -sf $HOME/.dotfiles/.vimrc $HOME/
 ln -sf $HOME/.dotfiles/.bashrc $HOME/
 ln -sf $HOME/.dotfiles/.cmds $HOME/
+ln -sf $HOME/.dotfiles/.zshrc $HOME/
 ln -sf /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/local/sbin/airport
