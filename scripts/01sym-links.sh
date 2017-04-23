e927001e689a0d13307b3eeb9f7fa229730dc922
Andrew Valentine <andrew.r.valentine@gmail.com>
2017-04-23 21:18:42 +0100

    remove old stuff, add new stuff

--- scripts/01sym-links.sh
+++ scripts/01sym-links.sh
@@ -4,7 +4,6 @@ ln -sf $HOME/.dotfiles/.bash_profile $HOME/
 ln -sf $HOME/.dotfiles/.bashrc $HOME/
 ln -sf $HOME/.dotfiles/.tmux.conf $HOME/
 ln -sf $HOME/.dotfiles/.vim $HOME/
-ln -sf $HOME/.dotfiles/.vimrc $HOME/
 ln -sf $HOME/.dotfiles/.cmds $HOME/
 ln -sf $HOME/.dotfiles/.zshrc $HOME/
 ln -sf $HOME/.dotfiles/.zsh $HOME/
@@ -12,6 +11,6 @@ ln -sf $HOME/.dotfiles/.zsh $HOME/
 case `uname` in
 	Darwin)
 sudo mkdir -p /usr/local/sbin
-sudo ln -sf /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/local/sbin
+sudo ln -sf /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/local/sbin/airport
 ;;
 esac
--- scripts/02set-env.sh
+++ scripts/02set-env.sh
@@ -3,20 +3,20 @@
 # Make sure we're in $HOME
 cd $HOME
 
-# Install Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git  ~/.vim/bundle/Vundle.vim
+# Install VimPlug
+echo "Installing VimPlug..."
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 case `uname` in
 	Darwin)
 
 # Install brew
+echo "Installing/updating brew..."
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# Update and upgrade brew packages
 brew update && brew upgrade
 
 # Install brew packages
-echo "Installing brew packages"
+echo "Installing brew packages..."
 
 # Install and configure git
 brew install git
@@ -30,65 +30,47 @@ brew install python
 brew install wget
 brew install tmux
 brew install htop
-brew install osxutils
-brew install ssh-copy-id
 brew install passpie
-brew install mas
-brew install peco
 brew install sf-pwgen
 brew install zsh
 brew install zsh-syntax-highlighting
-brew install macvim --env-std --with-override-system-vim
+brew install vim --env-std --with-override-system-vim
 brew install diff-so-fancy
+brew install diffoscope
+brew install ripgrep
+brew install ranger
+brew install nmap
+brew install cmus
 brew tap caskroom/cask
 
 # Install pip packages
-echo "Installing pip packages"
+echo "Installing pip packages..."
 pip install --upgrade pip setuptools
 pip install csvkit
 pip install virtualenv virtualenvwrapper
-pip install gitsome
 pip install ipython
 
 # Setup VirtualEnv space
-echo "Setting up VirtualEnv"
+echo "Setting up VirtualEnv..."
 source ~/.bash_profile
 mkdir -p $WORKON_HOME
 
-# Install The Luggage
-echo "Installing The Luggage"
-git clone https://github.com/unixorn/luggage.git ~/Downloads/the-luggage
-cd ~/Downloads/the-luggage
-make bootstrap_files
-cd ~
-/bin/rm -rf Downloads/the-luggage
-
-# Install Autopkg
-echo "Installing Autopkg"
-git clone https://github.com/autopkg/autopkg.git ~/Downloads/autopkg
-cd ~/Downloads/autopkg/
-sudo /bin/sh Scripts/install.sh
-/usr/local/bin/autopkg repo-add recipes timsutton-recipes hjuutilainen-recipes killahquam-recipes cgerke-recipes rtrouton-recipes jleggat-recipes andrewvalentine-recipes scriptingosx-recipes homebysix-recipes arubdesu-recipes eholtham-recipes triti-recipes jaharmi-recipes
-/usr/local/bin/autopkg run GoogleChrome.install Slack.install XQuartz.install iTerm2.install MunkiAdmin.install SuspiciousPackageApp.install DeployStudioAdmin.install
-cd ~
-/bin/rm -rf ~/Downloads/autopkg
-
 # Clone my repos
-git clone https://github.com/autopkg/andrewvalentine-recipes.git
+echo "Cloning personal repos..."
+git clone https://github.com/autopkg/andrewvalentine-recipes.git $HOME/repos/andrewvalentine-recipes
 git clone git@bitbucket.org:andrewvalentine/.passpie.git
-git clone git clone git@bitbucket.org:andrewvalentine/.todolist.git
 
 # Set shell to brew zsh
+echo "Setting shell to zsh..."
 sudo dscl . -create /Users/$USER UserShell /usr/local/bin/zsh
 
 # Clone zsh-git-prompt
-git clone https://github.com/olivierverdier/zsh-git-prompt.git ~/.zsh/zsh-git-prompt
+echo "Setting up zsh-git-prompt..."
+git clone https://github.com/olivierverdier/zsh-git-prompt.git $HOME/.zsh/zsh-git-prompt
 
 # Setup diff-so-fancy
+echo "Setting up diff-so-fancy..."
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
-
-git config --local user.name "Andrew Valentine"
-git config --local user.email andrew.r.valentine@gmail.com
 
 ;;
 	Linux)
@@ -99,19 +81,22 @@ wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py --user
 
 # Setup zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.local/zsh-syntax-highlighting
+echo "Setting zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.local/zsh-syntax-highlighting
 
 # Set zsh-git-prompt
-git clone https://github.com/olivierverdier/zsh-git-prompt.git ~/.zsh/zsh-git-prompt
+echo "Setting up zsh-git-prompt..."
+git clone https://github.com/olivierverdier/zsh-git-prompt.git $HOME/zsh/zsh-git-prompt
 
 # Setup diff-so-fancy
-curl https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/diff-so-fancy --output ~/.local/bin/diff-so-fancy
-curl https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/diff-highlight/diff-highlight --output ~/.local/bin/diff-highlight
-curl https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/lib/diff-so-fancy.pl --output ~/.local/bin/lib/diff-so-fancy.pl --create-dirs
+echo "Setting up diff-so-fancy..."
+curl https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/diff-so-fancy --output $HOME/.local/bin/diff-so-fancy --create-dirs
+curl https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/diff-highlight/diff-highlight --output $HOME/.local/bin/diff-highlight
+curl https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/lib/diff-so-fancy.pl --output $HOME/.local/bin/lib/diff-so-fancy.pl --create-dirs
 
-chmod +x ~/.local/bin/diff-so-fancy
-chmod +x ~/.local/bin/diff-highlight
-chmod +x ~/.local/bin/lib/diff-so-fancy.pl
+chmod +x $HOME/.local/bin/diff-so-fancy
+chmod +x $HOME/.local/bin/diff-highlight
+chmod +x $HOME/.local/bin/lib/diff-so-fancy.pl
 
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 
