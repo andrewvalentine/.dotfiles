475fa6a3a56afb655c7e0ec18e6e236383a17b9d
Andrew Valentine <andrew.r.valentine@gmail.com>
2020-01-07 09:07:28 +0000

    see comments in file

--- .zsh/history.zsh
+++ .zsh/history.zsh
@@ -2,3 +2,18 @@
 HISTSIZE=10000
 SAVEHIST=10000
 HISTFILE=~/.zsh_history
+
+# share history across multiple zsh sessions
+setopt SHARE_HISTORY
+# append to history
+setopt APPEND_HISTORY
+# adds commands as they are typed, not at shell exit
+setopt INC_APPEND_HISTORY
+# expire duplicates first
+setopt HIST_EXPIRE_DUPS_FIRST
+# do not store duplications
+setopt HIST_IGNORE_DUPS
+#ignore duplicates when searching
+setopt HIST_FIND_NO_DUPS
+# removes blank lines from history
+setopt HIST_REDUCE_BLANKS
