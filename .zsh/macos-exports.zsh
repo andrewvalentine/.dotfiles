6a4e63fc67fdc03677d29352ca0bf25e22928ebc
Andrew Valentine <andrew.r.valentine@gmail.com>
2020-01-07 09:12:09 +0000

    rename and update virtualenvwrapper python

--- .zsh/macos-exports.zsh
+++ .zsh/macos-exports.zsh
@@ -32,5 +32,5 @@ export GEM_HOME=$HOME/.gems
 
 # Virtualenv settings
 export WORKON_HOME=$HOME/Envs
-export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+export VIRTUALENVWRAPPER_PYTHON=`which python`
 source /usr/local/bin/virtualenvwrapper.sh
